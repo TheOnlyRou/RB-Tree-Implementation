ddad37d6e89fc43675fb5fa124f51c569a8d7736
MaRouane <34210209+TheOnlyRou@users.noreply.github.com>
2019-04-22 21:05:41 +0200

    what was saved

--- src/Main/RB_Tree.java
+++ src/Main/RB_Tree.java
@@ -38,7 +38,7 @@ public class RB_Tree {
         return key;
     }    
     
-        public Node insert(Node h,int key,String val){
+    public Node insert(Node h,int key,String val){
         if(h==null)
             return new Node(key,val,RED);
         
@@ -49,96 +49,10 @@ public class RB_Tree {
         else if(cmp>0)
             h.right = insert(h.right, key, val);
         else
-<<<<<<< HEAD
-<<<<<<< HEAD
             h.val=val;        
         return h;
     }
-        public void delete(float key) {
-        if (0.0f == key) throw new IllegalArgumentException("argument to delete() is null");
-        if (!contains(key)) return;
- 
-        // if both children of root are black, set root to red
-        if (!isRed(root.left) && !isRed(root.right))
-            root.color = RED;
- 
-        root = delete(root, key);
-        if (!isEmpty()) root.color = BLACK;
-        // assert check();
-    }
- 
-    private Node delete(Node h, float key) {
-        if (h.compareTo(key) < 0)  {
-            if (!isRed(h.left) && !isRed(h.left.left))
-                h = moveRedLeft(h);
-            h.left = delete(h.left, key);
-        }
-        else {
-            if (isRed(h.left))
-                h = rotateRight(h);
-            if (h.compareTo(key) == 0 && (h.right == null))
-                return null;
-            if (!isRed(h.right) && !isRed(h.right.left))
-                h = moveRedRight(h);
-            if (h.compareTo(key) == 0) {
-                Node x = min(h.right);
-                h.key = x.key;
-                h.val = x.val;
-                h.right = deleteMin(h.right);
-            }
-            else h.right = delete(h.right, key);
-        }
-        return balance(h);
-    }
-    
-    private Node balance(Node h) {
- 
-        if (isRed(h.right))                      h = rotateLeft(h);
-        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
-        if (isRed(h.left) && isRed(h.right))     flipColors(h);
- 
-=======
-            h.val=val; 
-        if(isRed(h.right)&&!isRed(h.left)){
-            h=rotateLeft(h);
-        }
-        if(isRed(h.left)&&isRed(h.left.left)){
-            h=rotateRight(h);
-        }
-        if(isRed(h.left)&&isRed(h.right)){
-            flipColors(h);
-        }
->>>>>>> parent of 256eb3e... maroooo
-=======
-            h.val=val; 
-        if(isRed(h.right)&&!isRed(h.left)){
-            h=rotateLeft(h);
-        }
-        if(isRed(h.left)&&isRed(h.left.left)){
-            h=rotateRight(h);
-        }
-        if(isRed(h.left)&&isRed(h.right)){
-            flipColors(h);
-        }
->>>>>>> parent of 1327ccf... Merge pull request #5 from Mohamed-Agwa/patch-2
-        h.size = size(h.left) + size(h.right) + 1;
-        
-        return h;
-    }
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-    private Node rotateRight(Node h) {
-        Node x = h.left;
-        h.left = x.right;
-        x.right = h;
-        x.color = x.right.color;
-        x.right.color = RED;
-        x.size = h.size;
-        h.size = size(h.left) + size(h.right) + 1;
-=======
-=======
->>>>>>> parent of 1327ccf... Merge pull request #5 from Mohamed-Agwa/patch-2
+
     public Node rotateLeft(Node h){
         Node x=h.right;
         h.right=x.left;
@@ -147,10 +61,6 @@ public class RB_Tree {
         x.left.color=RED;
         x.size=h.size;
         h.size=size(h.left)+size(h.right)+1;
-<<<<<<< HEAD
->>>>>>> parent of 256eb3e... maroooo
-=======
->>>>>>> parent of 1327ccf... Merge pull request #5 from Mohamed-Agwa/patch-2
         return x;
     }
     public Node rotateRight(Node h){
@@ -191,100 +101,5 @@ public class RB_Tree {
         else
             h.val=val;        
         return h;
-<<<<<<< HEAD
-<<<<<<< HEAD
     }
- public boolean isEmpty() {
-        return root == null;
-    }
- public void deleteMin() {
-        if (isEmpty()) throw new NoSuchElementException("BST underflow");
-        if (!isRed(root.left) && !isRed(root.right))
-            root.color = RED;
- 
-        root = deleteMin(root);
-        if (!isEmpty()) root.color = BLACK;
-    }
-    private Node deleteMin(Node h) {
-        if (h.left == null)
-            return null;
- 
-        if (!isRed(h.left) && !isRed(h.left.left))
-            h = moveRedLeft(h);
- 
-        h.left = deleteMin(h.left);
-        return balance(h);
-    }
-    public void deleteMax() {
-        if (isEmpty()) throw new NoSuchElementException("BST underflow");
-        if (!isRed(root.left) && !isRed(root.right))
-            root.color = RED;
- 
-        root = deleteMax(root);
-        if (!isEmpty()) root.color = BLACK;
-       
-    }
-    private Node deleteMax(Node h) {
-        if (isRed(h.left))
-            h = rotateRight(h);
- 
-        if (h.right == null)
-            return null;
- 
-        if (!isRed(h.right) && !isRed(h.right.left))
-            h = moveRedRight(h);
- 
-        h.right = deleteMax(h.right);
- 
-        return balance(h);
-    }
- 
-    public float min() {
-        if (isEmpty()) throw new NoSuchElementException("calls min() with empty symbol table");
-        return min(root).key;
-    }
-    private Node min(Node x) {
-        if (x.left == null) return x;
-        else                return min(x.left);
-    }
- 
-    public float max() {
-        if (isEmpty()) throw new NoSuchElementException("calls max() with empty symbol table");
-        return max(root).key;
-    }
-    private Node max(Node x) {
-        // assert x != null;
-        if (x.right == null) return x;
-        else                 return max(x.right);
-    }
-     private int size(Node x) {
-        if (x == null) return 0;
-        return x.size;
-    }
-    public int size() {
-        return size(root);
-    }
-    public boolean contains(float key) {
-        return get(key) != null;
-    }
-     public String get(float key) {
-        if (0.0f == key) throw new IllegalArgumentException("argument to get() is null");
-        return get(root, key);
-    }
- 
-    private String get(Node x, float key) {
-        while (x != null) {
-            int cmp = x.compareTo(key);
-            if      (cmp < 0) x = x.left;
-            else if (cmp > 0) x = x.right;
-            else              return x.val;
-        }
-        return null;
-    }
-=======
-    }    
->>>>>>> parent of 256eb3e... maroooo
-=======
-    }    
->>>>>>> parent of 1327ccf... Merge pull request #5 from Mohamed-Agwa/patch-2
 }
