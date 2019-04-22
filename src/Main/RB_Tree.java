0af724bc6579da54034692918a2cca589827d520
MaRouane <34210209+TheOnlyRou@users.noreply.github.com>
2019-04-22 20:44:12 +0200

    Update RB_Tree.java

--- src/Main/RB_Tree.java
+++ src/Main/RB_Tree.java
@@ -1,39 +1,55 @@
 package Main;
- 
-import java.util.NoSuchElementException;
-import java.lang.String;
- 
- 
+
+import static java.lang.Math.pow;
+
+
 public class RB_Tree {
     public Node root;
     public static final boolean RED   = true;
     public static final boolean BLACK = false;
-   
+    
     public boolean isRed(Node x){
         if(x==null)
             return false;
         return x.color==RED;
     }
-    public void insert(int key,String val){
-        if(val==null){
-            return;
-        }
-        root=insert(root,key,val);
-        root.color=BLACK;        
+    
+    public int size(Node x){
+        if(x==null)
+            return 0;
+        return x.size;
     }
-   
-   
-    public Node insert(Node h,int key,String val){
+    
+    public boolean isEmpty(){
+        return root==null;
+    }
+    
+    public float generateKey(String val)
+    {
+        float key = 0;
+        val = val.toUpperCase();
+        char[] arr = val.toCharArray();
+        for(int i=0; i<val.length();i++)
+        {
+            System.out.println(arr[i]);
+            key += arr[i]/pow(10,2*i);
+        }
+        System.out.println(key);
+        return key;
+    }    
+    
+        public Node insert(Node h,int key,String val){
         if(h==null)
             return new Node(key,val,RED);
-       
+        
         int cmp= h.compareTo(key);
-       
+        
         if(cmp<0)
             h.left=insert(h.left,key,val);
         else if(cmp>0)
             h.right = insert(h.right, key, val);
         else
+<<<<<<< HEAD
             h.val=val;        
         return h;
     }
@@ -80,9 +96,23 @@ public class RB_Tree {
         if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
         if (isRed(h.left) && isRed(h.right))     flipColors(h);
  
+=======
+            h.val=val; 
+        if(isRed(h.right)&&!isRed(h.left)){
+            h=rotateLeft(h);
+        }
+        if(isRed(h.left)&&isRed(h.left.left)){
+            h=rotateRight(h);
+        }
+        if(isRed(h.left)&&isRed(h.right)){
+            flipColors(h);
+        }
+>>>>>>> parent of 256eb3e... maroooo
         h.size = size(h.left) + size(h.right) + 1;
+        
         return h;
     }
+<<<<<<< HEAD
     
     private Node rotateRight(Node h) {
         Node x = h.left;
@@ -92,41 +122,57 @@ public class RB_Tree {
         x.right.color = RED;
         x.size = h.size;
         h.size = size(h.left) + size(h.right) + 1;
+=======
+    public Node rotateLeft(Node h){
+        Node x=h.right;
+        h.right=x.left;
+        x.left=h;
+        x.color=x.left.color;
+        x.left.color=RED;
+        x.size=h.size;
+        h.size=size(h.left)+size(h.right)+1;
+>>>>>>> parent of 256eb3e... maroooo
         return x;
     }
-    private Node rotateLeft(Node h) {
-        Node x = h.right;
-        h.right = x.left;
-        x.left = h;
-        x.color = x.left.color;
-        x.left.color = RED;
-        x.size = h.size;
-        h.size = size(h.left) + size(h.right) + 1;
+    public Node rotateRight(Node h){
+        Node x=h.left;
+        h.left=x.right;
+        x.right=h;
+        x.color=x.right.color;
+        x.right.color=RED;
+        x.size=h.size;
+        h.size=size(h.left)+size(h.right);
         return x;
     }
- 
-    private void flipColors(Node h) {
-        h.color = !h.color;
-        h.left.color = !h.left.color;
-        h.right.color = !h.right.color;
+    public void flipColors(Node h){
+        h.color=!h.color;
+        h.left.color=!h.left.color;
+        h.right.color=!h.right.color;
     }
-   
-     private Node moveRedLeft(Node h) {
-        flipColors(h);
-        if (isRed(h.right.left)) {
-            h.right = rotateRight(h.right);
-            h = rotateLeft(h);
-            flipColors(h);
+    
+    public void insert(String val){
+        if(val==null){
+            return;
         }
-        return h;
+        float key = generateKey(val);
+        root=insert(root,key,val);
+        root.color=BLACK;        
     }
-    private Node moveRedRight(Node h) {
-        flipColors(h);
-        if (isRed(h.left.left)) {
-            h = rotateRight(h);
-            flipColors(h);
-        }
+    
+    public Node insert(Node h, float key, String val){
+        if(h==null)
+            return new Node(key,val,RED);
+        
+        int cmp= h.compareTo(key);
+        
+        if(cmp<0)
+            h.left=insert(h.left,key,val);
+        else if(cmp>0)
+            h.right = insert(h.right, key, val);
+        else
+            h.val=val;        
         return h;
+<<<<<<< HEAD
     }
  public boolean isEmpty() {
         return root == null;
@@ -215,4 +261,7 @@ public class RB_Tree {
         }
         return null;
     }
+=======
+    }    
+>>>>>>> parent of 256eb3e... maroooo
 }
